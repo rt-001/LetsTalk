8f334bbd18a1053b71f137b8a55c0807412caf78
Romit Tiwari <romittiwari100@gmail.com>
2022-08-24 19:17:45 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { chats } = require("./data/data");
+const app = express(express);
+const dotenv = require("dotenv");
+dotenv.config();
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => {
+  console.log("Backend is running.");
+});
+Footer;
