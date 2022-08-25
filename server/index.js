a11ef6f61169fc3a0973b03431ba71234d3c527d
Romit Tiwari <romittiwari100@gmail.com>
2022-08-25 17:04:49 +0530

    MONGODB Connected

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const { chats } = require("./data/data");
 const app = express(express);
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 dotenv.config();
+connectDB();
 app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
@@ -10,3 +12,6 @@ const PORT = process.env.PORT || 8800;
 app.listen(PORT, () => {
   console.log("Backend is running.");
 });
+
+// romittiwari
+// romittiwari
