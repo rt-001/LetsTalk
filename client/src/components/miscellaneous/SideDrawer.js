4cc2cbf4450a51e677cb1ff82862fe52d6bc45bc
Romit Tiwari <romittiwari100@gmail.com>
2022-09-04 19:57:15 +0530

    Notification Done

--- client/src/components/miscellaneous/SideDrawer.js
+++ client/src/components/miscellaneous/SideDrawer.js
@@ -29,6 +29,7 @@ import NotificationBadge from "react-notification-badge";
 import { Effect } from "react-notification-badge";
 import { ChatState } from "../../Context/ChatProvider";
 import ProfileModal from "./ProfileModal";
+import { getSender } from "../../config/ChatLogics";
 import UserListItem from "../userAvatar/UserListItem";
 function SideDrawer() {
   const [search, setSearch] = useState("");
@@ -79,6 +80,7 @@ function SideDrawer() {
 
       setLoading(false);
       setSearchResult(data);
+      // console.log("user jo bar", data);
     } catch (error) {
       toast({
         title: "Error Occured!",
@@ -146,17 +148,35 @@ function SideDrawer() {
         <div>
           <Menu>
             <MenuButton p={1}>
-              <NotificationBadge />
+              <NotificationBadge
+                count={notification.length}
+                effect={Effect.SCALE}
+              />
               <BellIcon fontSize="2xl" m={1} />
             </MenuButton>
-            <MenuList pl={2}></MenuList>
+            <MenuList pl={2}>
+              {!notification.length && "No New Messages"}
+              {notification.map((notif) => (
+                <MenuItem
+                  key={notif._id}
+                  onClick={() => {
+                    setSelectedChat(notif.chat);
+                    setNotification(notification.filter((n) => n !== notif));
+                  }}
+                >
+                  {notif.chat.isGroupChat
+                    ? `New Message in ${notif.chat.chatName}`
+                    : `New Message from ${getSender(user, notif.chat.users)}`}
+                </MenuItem>
+              ))}
+            </MenuList>
           </Menu>
           <Menu>
             <MenuButton as={Button} bg="white" rightIcon={<ChevronDownIcon />}>
               <Avatar size="sm" cursor="pointer" name={name} src={user.pic} />
             </MenuButton>
             <MenuList>
-              <ProfileModal>
+              <ProfileModal user={user}>
                 <MenuItem>My Profile</MenuItem>
               </ProfileModal>
               <MenuDivider></MenuDivider>
